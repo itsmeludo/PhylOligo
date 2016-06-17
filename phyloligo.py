e8593d157ef5e8eac41ac9c90c21b62ade5d97de
Ludovic Mallet <lumallet@cylindre.toulouse.inra.fr>
2016-06-17 10:43:57 +0200

    initial creation of project on github

---
+++ phyloligo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+### Author: Ludovic V. Mallet, PhD
+### 2016.03.22
+### licence: GPLv3
+### Version: Alpha.1
+### Garanteed with misatkes. <- Including this one.
+
+
+# This program builds a phylotree based on oligonucleotide profiles (microcomposition) distances for sequences given as arguments.
+# See the help by calling the program without any argument.
+
+
+#dependencies: 
+  #Biopython
+  #numpy
+  #cython
