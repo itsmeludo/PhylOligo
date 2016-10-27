151df1826799f296fad3e9af05b3cd9ec81ba027
Ludovic Mallet <lmallet@genotoul2.(none)>
2016-10-27 11:28:10 +0200

    changes python version, execution rights , qnd arguments pass-through for the scoop bash wrapper

--- phyloligo.sh
+++ phyloligo.sh
@@ -8,5 +8,7 @@
 #$ -l h_vmem=8G
 
 
-python -m scoop -n 8 phyloligo.py  -i ../PhylOligo_Mor/data/M.oryzae_TH12.fasta -k 5 -d JSD -o ../PhylOligo_Mor/results/M.oryzae_TH12_JSD_scoop2.mat
+python333 -m scoop -n $NSLOTS phyloligo.py $@
+
+#-i ../PhylOligo_Mor/data/M.oryzae_TH12.fasta -k 5 -d JSD -o ../PhylOligo_Mor/results/M.oryzae_TH12_JSD_scoop2.mat
 
