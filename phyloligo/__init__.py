110bf4484946d047e8bed3a7a6f1658bb66b27af
T-B-F <tristan.bitard.feildel@gmail.com>
2016-11-30 08:13:42 +0900

    some cleaning up

--- phyloligo/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.1"
-__release__ = __version__  + '-dev' # comment out '-dev' before a release
-
-from phyloligo.core.phylodist import compute_distances
-from phyloligo.core.phylofreq import compute_frequencies
